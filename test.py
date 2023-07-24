806d2c524aa3f196c194b50a7cb268b08423355f
JenaMischk <71723649+JenaMischk@users.noreply.github.com>
2023-07-24 15:20:46 +0100

    Switch Python test script from Firefox to Chrome

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-browser = webdriver.Firefox()
+browser = webdriver.Chrome()
 
 browser.get('http://www.yahoo.com')
 assert 'Yahoo' in browser.title
